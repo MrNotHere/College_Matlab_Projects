bac8b2ab60298986748ec553c213cccf11f11e97
MrNotHere <gattod115@gmail.com>
2026-02-07 01:44:04 -0500

    Update Satellite_Simulation_2D_Test.m

--- Satellite_Simulation_2D_Test.m
+++ Satellite_Simulation_2D_Test.m
@@ -2,10 +2,11 @@
 Input Arguments:
 obj (array)
 Obj(1) = Object's  mass (kg)
-Obj(2) = Object's inital velocity (km/s) 
-Obj(3) = Object's Alitude (km)
-S_O = distance between sun and satellite (Km) 
-Fine if there is a way to calcuate this value
+Obj(2) = inital x pos (km)
+Obj(3) = inital y pos (km)
+Obj(4) = inital z pos (km) 
+Obj(5) = Object's inital velocity (km/s)
+Obj(6) = Object's inital accel (km/s)
 
 Output Arguements:
 F (Array)
@@ -13,9 +14,14 @@ F(1) = magnitude of force (N)
 F(2) = x component of force(N)
 F(3) = y component of force(N)
 F(4) = z component of force(N)
+
+CDA (Coordinate directional angles)
+
+v (x, y, z components of velocity) 
+a (x, y, z components of accleration)
+
 %}
 
-Obj_1 = [1,200, 310];
-S_O_1 = 149.6E6 + 1000;
-F_1 = Satellite_Simulation_2D(Obj_1, S_O_1);
+Obj_1 = [1, 300, 280, 180, 100, 50];
+[F_1, CDA_1, v_1, a_1] = Satellite_Simulation_2D(Obj_1)
 
