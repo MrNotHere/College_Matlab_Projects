8c534473b23950771ee62f7d758461592c663c95
MrNotHere <gattod115@gmail.com>
2026-02-07 11:14:33 -0500

    Update Satellite_Simulation_3D.m

--- Satellite_Simulation_3D.m
+++ Satellite_Simulation_3D.m
@@ -1,14 +1,17 @@
-function [F, CDA, v, a] = Satellite_Simulation_3D(Obj)
+function [F] = Satellite_Simulation_3D(Obj, accel)
 %determines the inital velocty magnitude as well as the x, y, z components
 %{
 Input Arguments:
-obj (array)
+Obj (array)
 Obj(1) = Object's  mass (kg)
 Obj(2) = inital x pos (km)
 Obj(3) = inital y pos (km)
 Obj(4) = inital z pos (km) 
-Obj(5) = Object's inital velocity (km/s)
-Obj(6) = Object's inital accel (km/s)
+
+accel (x, y, z components of accel) (m/s^2)
+accel(1) = x 
+accel(2) = y
+accel(3) = z
 
 Output Arguements:
 F (Array)
@@ -19,7 +22,6 @@ F(4) = z component of force(N)
 
 CDA (Coordinate directional angles)
 
-v (x, y, z components of velocity) 
 a (x, y, z components of accleration)
 
 %}
@@ -33,16 +35,11 @@ G = 6.6743E-11; % gravatational constant (m^3kg^-1s^-2)
 alt = sqrt(Obj(2)^2 + Obj(3)^2 + Obj(4)^2);
 r = E_radius + alt; % Satellite radius from earth center (km)
 u_r = [Obj(2)/r,Obj(3)/r, Obj(4)/r];
-
-F(1) = G*(E_mass.*Obj(1))./(r/1000).^2; % Magnitude of force and its components (N)
-F(2) = F(1).*u_r(1); 
-F(3) = F(1).*u_r(2); 
-F(4) = F(1).*u_r(3);
-
-CDA = [acos(F(2)./F(1)), acos(F(3)./F(1)), acos(F(4)./F(1))];
-v = [Obj(5).*u_r(1), Obj(5).*u_r(2), Obj(5).*u_r(3)];
-a = [Obj(6).*u_r(1), Obj(6).*u_r(2), Obj(6).*u_r(3)];
+F_E(1) = (G*E_mass.*Obj(1))./((r*1000).^2); % Magnitude of force and its components (N)
+F_E(2) = F_E(1).*u_r(1); 
+F_E(3) = F_E(1).*u_r(2); 
+F_E(4) = F_E(1).*u_r(3);
+F_accel = Obj(1).*accel;
+F = F_accel + F_E(2:4);
 
 end
-
-
