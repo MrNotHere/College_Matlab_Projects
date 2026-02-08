9a451ef0e9d751599eb5dfd3a494a2a7fc66ef1b
MrNotHere <gattod115@gmail.com>
2026-02-08 12:21:17 -0500

    Update Satellite_Simulation_3D.m

--- Satellite_Simulation_3D.m
+++ Satellite_Simulation_3D.m
@@ -1,4 +1,4 @@
-function [F] = Satellite_Simulation_3D(Obj, accel)
+function [F] = Satellite_Simulation_3D(Obj)
 %determines the inital velocty magnitude as well as the x, y, z components
 %{
 Input Arguments:
@@ -7,6 +7,12 @@ Obj(1) = Object's  mass (kg)
 Obj(2) = inital x pos (km)
 Obj(3) = inital y pos (km)
 Obj(4) = inital z pos (km) 
+Obj(5) = inital x vel (km/s)
+Obj(6) = inital y vel (km/s)
+Obj(7) = inital z vel (km/s)
+Obj(8) = inital x accel (km/s^2)
+Obj(9) = inital y accel (km/s^2)
+Obj(10) = inital z accel (km/s^2)
 
 accel (x, y, z components of accel) (m/s^2)
 accel(1) = x 
@@ -34,12 +40,12 @@ G = 6.6743E-11; % gravatational constant (m^3kg^-1s^-2)
 %Variables 
 alt = sqrt(Obj(2)^2 + Obj(3)^2 + Obj(4)^2);
 r = E_radius + alt; % Satellite radius from earth center (km)
-u_r = [Obj(2)/r,Obj(3)/r, Obj(4)/r];
+u_r = Obj(2:4)/r;
 F_E(1) = (G*E_mass.*Obj(1))./((r*1000).^2); % Magnitude of force and its components (N)
 F_E(2) = F_E(1).*u_r(1); 
 F_E(3) = F_E(1).*u_r(2); 
 F_E(4) = F_E(1).*u_r(3);
-F_accel = Obj(1).*accel;
+F_accel = Obj(1).*Obj(8:10);
 F = F_accel + F_E(2:4);
 
 end
