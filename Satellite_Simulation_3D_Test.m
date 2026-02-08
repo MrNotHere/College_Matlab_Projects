80220c27e71f1b9e62d0e8b942762008b1556c41
MrNotHere <gattod115@gmail.com>
2026-02-08 12:21:56 -0500

    Update Satellite_Simulation_3D_Test.m

--- Satellite_Simulation_3D_Test.m
+++ Satellite_Simulation_3D_Test.m
@@ -1,31 +1,31 @@
 %{
 Input Arguments:
-obj (array)
+Obj (array)
 Obj(1) = Object's  mass (kg)
 Obj(2) = inital x pos (km)
 Obj(3) = inital y pos (km)
 Obj(4) = inital z pos (km) 
-Obj(5) = Object's inital velocity (km/s)
-Obj(6) = Object's inital accel (km/s)
+Obj(5) = inital x vel (km/s)
+Obj(6) = inital y vel (km/s)
+Obj(7) = inital z vel (km/s)
+Obj(8) = inital x accel (km/s^2)
+Obj(9) = inital y accel (km/s^2)
+Obj(10) = inital z accel (km/s^2)
 
-Output Arguements:
-F (Array)
-F(1) = magnitude of force (N)
-F(2) = x component of force(N)
-F(3) = y component of force(N)
-F(4) = z component of force(N)
-
-CDA (Coordinate directional angles)
-
-v (x, y, z components of velocity) 
-a (x, y, z components of accleration)
-
+accel (x, y, z components of accel) (m/s^2)
+accel(1) = x 
+accel(2) = y
+accel(3) = z
 %}
 
-Obj_1 = [1, 300, 280, 180]; 
-accel_ = [25, 50, 75]; %inital velocity 
-vel_i = [20, 40, 60]; %initial aceel
-[F_1, CDA_1, a_1] = Satellite_Simulation_3D(Obj_1)
+Obj_i = [1, 300, 280, 180, 20, 40, 60, 25, 50, 75];
+[F_1] = Satellite_Simulation_3D(Obj_i);
+
+%Time of 1 second has past
+Obj_f = 1:10;
+Obj_f(8:10) = F_1./Obj_i(1);
+Obj_f(5:7) = Obj_i(5:7) + 1.*Obj_i(8:10);
+Obj_f(2:4) = Obj_i(2:4) + 1.*Obj_i(5:7) + .5.*Obj_i(8:10).^2;
 
 
 
